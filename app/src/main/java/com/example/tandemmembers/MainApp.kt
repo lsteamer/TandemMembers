5b9a23302e983c4721e3a7d8362c57c740846999
Antonio Gonzalez <lsteamer@gmail.com>
2020-10-16 12:19:09 +0200

    Added DI framework with Koin

---
+++ app/src/main/java/com/example/tandemmembers/MainApp.kt
@@ -0,0 +1,20 @@
+package com.example.tandemmembers
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MainApp)
+            modules(
+                listOf(
+                    tandemModule
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tandemmembers/module.kt
@@ -0,0 +1,7 @@
+package com.example.tandemmembers
+
+import org.koin.dsl.module
+
+val tandemModule = module {
+
+}
